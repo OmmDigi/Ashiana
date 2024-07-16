06586f8fdbbd9d5625a2ca29a1a1c8c13a5bf964
OmmDigi <ommdigitalsolution@gmail.com>
2024-07-16 18:40:34 +0530

    add perment redirect

--- next.config.mjs
+++ next.config.mjs
@@ -20,6 +20,16 @@ const nextConfig = {
         destination: "/blog",
         permanent: true,
       },
+      {
+        source: "/projects/residential",
+        destination: "/projects?catname=residential",
+        permanent: true,
+      },
+      {
+        source: "/services/paint",
+        destination: "/services/paint-and-wallpaper",
+        permanent: true,
+      },
     ];
   },
 };
